f5d2a342118245aebe9a7ccb5b065f61172a83de
Ronniel de Ramos <ronnielderamos@gmail.com>
2021-11-13 09:59:37 +0800

    trade function

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -7,14 +7,19 @@ class TradesController < ApplicationController
   end
 
   def create
-    @user = current_user
-    @trade = Trade.new(trade_params)
-    if @trade.save
-      flash[:notice] = "trade added"
-      render 'trades/new'
-    else
-      render 'new'
-    end
+    @trade = Trade.new(trade_type: params[:trade_type], quantity: params[:quantity], user_id: params[:user_id],
+                        user_stock_id: params[:user_stock_id], total_price: params[:total_price])
+    @trade.save
+    flash[:notice] = "success"
+    redirect_to my_portfolio_path
+    # @user = current_user
+    # @trade = Trade.new(trade_params)
+    # if @trade.save
+    #   flash[:notice] = "trade added"
+    #   render 'trades/new'
+    # else
+    #   render 'new'
+    # end
   end
 
   def cart
@@ -25,13 +30,14 @@ class TradesController < ApplicationController
     user_stock_id = params[:user_stock_id]
     trade_type = params[:trade_type]
     quantity = params[:quantity]
-    @trade = Trade.new(user_id: params[:user_id], user_stock_id: params[:user_stock_id], trade_type: params[:trade_type], quantity: params[:quantity])
-    @total_price  = @stock.last_price * @trade.quantity
+    total_price = @stock.last_price * quantity.to_d
+    @trade = Trade.new(user_id: params[:user_id], user_stock_id: params[:user_stock_id], trade_type: params[:trade_type], quantity: params[:quantity], total_price: total_price)
+    # @total_price  = @stock.last_price * @trade.quantity
     render 'trades/new'
   end
 
-  private
-   def trade_params
-    params.require(:trade).permit(:user_id, :user_stock_id, :trade_type, :quantity)
-   end
+  # private
+  #  def trade_params
+  #   params.require(:trade).permit(:user_id, :user_stock_id, :trade_type, :quantity, :total_price)
+  #  end
 end
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,4 +1,6 @@
 class Trade < ApplicationRecord
   belongs_to :user
   belongs_to :user_stock
+
+  validates :trade_type, :quantity, :user_id, :user_stock_id, :total_price, presence: true
 end
